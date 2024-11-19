c81872f743b11e703a3377f95f980e5ae45ba229
YASHJADON5 <yash-jadon>
2024-11-19 15:46:02 +0530

    done changes in hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,6 +3,8 @@ import { gsap } from "gsap";
 import herobg from "../assets/herobg.png";
 import pattern1 from '../assets/pattern1.png';
 import { Link } from "react-router-dom";
+import { TextRevealCard, TextRevealCardTitle, TextRevealCardDescription } from "../Components/ui/TextRevealCard";
+
 
 const Hero = () => {
   useEffect(() => {
@@ -25,17 +27,30 @@ const Hero = () => {
     >
       <div className="container px-4 sm:px-8 mx-auto max-w-[77rem]">
         <div className="max-w-xl text-left text-white">
-          <h1 className="mb-6 text-4xl font-bold leading-tight sm:text-5xl md:text-6xl mf text-dark">
+          {/* <h1 className="mb-6 text-4xl font-bold leading-tight sm:text-5xl md:text-6xl mf text-dark">
             Luxury You Can Trust, Softness You Can Feel
-          </h1>
-          <p className="mb-6 text-lg text-gray-700 md:text-xl">
+          </h1> */}
+          {/* <p className="mb-6 text-lg text-gray-700 md:text-xl">
             From everyday use to special moments, our tissues provide the perfect blend of elegance and functionality.
-          </p>
+          </p> */}
+          <div className="flex items-center justify-center w-[100%]">
+      <TextRevealCard
+        text="Sof Scroll"
+        revealText="Luxury You Can Trust, Softness You Can Feel"
+        className="w-96"
+      >
+        {/* Add optional children here */}
+        {/* <TextRevealCardTitle>Exciting Title</TextRevealCardTitle> */}
+        {/* <TextRevealCardDescription> */}
+          {/* This is an awesome description for the card. */}
+        {/* </TextRevealCardDescription> */}
+        </TextRevealCard>
+        </div>
           {/* <Link to={'/all-products'} className="Btn">
           </Link> */}
           <Link
             to={'/all-products'}
-            className="inline-grid w-40 px-10 py-2 overflow-hidden text-center text-black transition-all duration-300 border rounded-full button border-dark hover:bg-dark hover:text-white"
+            className="ml-36 inline-grid w-40 px-10 py-2 overflow-hidden text-center text-black transition-all duration-300 border rounded-full button border-dark hover:bg-dark hover:text-white"
           >
             <span>Shop Now</span>
           </Link>
