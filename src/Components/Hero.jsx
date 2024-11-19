0f9830152c1cd88a1a8230548a1b902a13b8f37f
Zoya774 <158023579+Zoya774@users.noreply.github.com>
2024-11-19 16:21:30 +0530

    update herobg

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,64 +1,145 @@
 import React, { useEffect } from "react";
+
 import { gsap } from "gsap";
+
 import { Link } from "react-router-dom";
+
 import { TextRevealCard, TextRevealCardTitle, TextRevealCardDescription } from "../Components/ui/TextRevealCard";
+
+import tisssuevideo from '../assets/tisssuevideo.mp4'
+
+
+
 
 
 const Hero = () => {
+
   useEffect(() => {
+
     const button = document.querySelector(".button");
+
     const span = button.querySelector("span");
+
     const tl = gsap.timeline({ paused: true });
 
+
+
     tl.to(span, { duration: 0.2, yPercent: -150, ease: "power2.in" });
+
     tl.set(span, { yPercent: 150 });
+
     tl.to(span, { duration: 0.2, yPercent: 0 });
 
+
+
     button.addEventListener("mouseenter", () => tl.play(0));
+
     return () => button.removeEventListener("mouseenter", () => tl.play(0));
+
   }, []);
 
+
+
   return ( 
+
     <div
+
       className="relative flex items-center h-screen bg-center bg-cover -top-20 md:top-0"
-      style={{ backgroundImage: `url(${herobg})` }}
+
+      
+
     >
+
+      <video
+
+      src={tisssuevideo}
+
+      autoPlay
+
+      loop
+
+      muted
+
+      className="absolute inset-0 w-full h-full object-cover"
+
+      ></video>
+
+
+
       <div className="container px-4 sm:px-8 mx-auto max-w-[77rem]">
+
         <div className="max-w-xl text-left text-white">
+
           {/* <h1 className="mb-6 text-4xl font-bold leading-tight sm:text-5xl md:text-6xl mf text-dark">
+
             Luxury You Can Trust, Softness You Can Feel
+
           </h1> */}
+
           {/* <p className="mb-6 text-lg text-gray-700 md:text-xl">
+
             From everyday use to special moments, our tissues provide the perfect blend of elegance and functionality.
+
           </p> */}
+
           <div className="flex items-center justify-center w-[100%]">
+
       <TextRevealCard
+
         text="Sof Scroll"
+
         revealText="Luxury You Can Trust, Softness You Can Feel"
+
         className="w-96"
+
       >
+
         {/* Add optional children here */}
+
         {/* <TextRevealCardTitle>Exciting Title</TextRevealCardTitle> */}
+
         {/* <TextRevealCardDescription> */}
+
           {/* This is an awesome description for the card. */}
+
         {/* </TextRevealCardDescription> */}
+
         </TextRevealCard>
+
         </div>
+
           {/* <Link to={'/all-products'} className="Btn">
+
           </Link> */}
+
           <Link
+
             to={'/all-products'}
-            className="ml-36 inline-grid w-40 px-10 py-2 overflow-hidden text-center text-black transition-all duration-300 border rounded-full button border-dark hover:bg-dark hover:text-white"
+
+            className=" ml-36 inline-grid w-40 px-10 py-2 overflow-hidden text-center text-black transition-all duration-300 border rounded-full button border-dark hover:bg-dark hover:text-white"
+
           >
+
             <span>Shop Now</span>
+
           </Link>
+
         </div>
+
       </div>
 
+
+
       {/* Decorative Pattern Image */}
+
       {/* <img src={pattern1} alt="Pattern" className="absolute right-0 hidden w-48 bottom-20 md:flex" /> */}
+
     </div>
+
   );
+
 };
+
+
 
 export default Hero;
