302f96656ed54fda924f2eeee597dde57fa4afa9
Zoya774 <158023579+Zoya774@users.noreply.github.com>
2024-11-19 14:37:21 +0530

    update changes

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from "react";
 import { gsap } from "gsap";
-import herobg from "../assets/herobg.png";
-import pattern1 from '../assets/pattern1.png';
 import { Link } from "react-router-dom";
+import herobg from "../assets/tisssuevideo.mp4";
 
 const Hero = () => {
   useEffect(() => {
@@ -18,30 +17,39 @@ const Hero = () => {
     return () => button.removeEventListener("mouseenter", () => tl.play(0));
   }, []);
 
-  return ( 
-    <div
-      className="relative flex items-center h-screen bg-center bg-cover -top-20 md:top-0"
-      style={{ backgroundImage: `url(${herobg})` }}
-    >
-      <div className="container px-4 sm:px-8 mx-auto max-w-[77rem]">
-        <div className="max-w-xl text-left text-white">
-          <h1 className="mb-6 text-4xl font-bold leading-tight sm:text-5xl md:text-6xl mf text-dark">
+  return (
+    <div className="relative flex items-center h-screen -top-20 md:top-0">
+      {/* Video Background */}
+      <div className="absolute h-screen bg-black opacity-75"></div>
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        src={herobg}
+        autoPlay
+        loop
+        muted
+      ></video>
+
+      {/* Content Overlay */}
+      <div className="container relative px-4 sm:px-8 mx-auto max-w-[77rem] text-white">
+        <div className="max-w-xl text-left">
+          <h1 className="mb-6 text-4xl font-bold leading-tight sm:text-5xl md:text-6xl text-light">
             Luxury You Can Trust, Softness You Can Feel
           </h1>
-          <p className="mb-6 text-lg text-gray-700 md:text-xl">
-            From everyday use to special moments, our tissues provide the perfect blend of elegance and functionality.
-          </p>
-          {/* <Link to={'/all-products'} className="Btn">
-          </Link> */}
+          {/* <p className="mb-6 text-lg text-gray-300 md:text-xl">
+            From everyday use to special moments, our tissues provide the
+            perfect blend of elegance and functionality.
+          </p> */}
           <Link
-            to={'/all-products'}
+            to={"/all-products"}
             className="inline-grid w-40 px-10 py-2 overflow-hidden text-center text-black transition-all duration-300 border rounded-full button border-dark hover:bg-dark hover:text-white"
           >
             <span>Shop Now</span>
           </Link>
         </div>
       </div>
-      <img src={pattern1} alt="Pattern" className="absolute right-0 hidden w-48 bottom-20 md:flex" />
+
+      {/* Decorative Pattern Image */}
+      {/* <img src={pattern1} alt="Pattern" className="absolute right-0 hidden w-48 bottom-20 md:flex" /> */}
     </div>
   );
 };
