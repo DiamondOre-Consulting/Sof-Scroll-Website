440c191afee66be0a2469b4ade79024f2da1e7c6
Akash Kumar Singh <itsakash18.06@gmail.com>
2024-11-22 15:42:44 +0530

    Color code updated and home page updated

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import pattern2 from "../assets/pattern2.png";
-import TextReveal from "@/components/ui/text-reveal";
+import AnimatedText from "./AnimatedText";
 
 const AboutUs = () => {
   return (
@@ -14,7 +14,8 @@ const AboutUs = () => {
           <div className="w-20 h-1 mx-auto md:w-52 bg-dark"></div>
 
           <div className="z-10 w-full mf">
-            <TextReveal text=" Our tissue paper conversion process turns large tissue rolls into consumer-ready products such as toilet paper, facial tissues, napkins, and paper towels. Using quality raw materials like wood pulp or recycled paper, the pulp is processed into continuous sheets, which are then slit, embossed, and perforated for convenience. Finally, automated machinery packages the tissue in various formats, ensuring quality and efficiency in a sustainable way to meet consumer needs." />
+            <AnimatedText text2={" Our tissue paper conversion process turns large tissue rolls into consumer-ready products such as toilet paper, facial tissues, napkins, and paper towels. Using quality raw materials like wood pulp or recycled paper, the pulp is processed into continuous sheets, which are then slit, embossed, and perforated for convenience. Finally, automated machinery packages the tissue in various formats, ensuring quality and efficiency in a sustainable way to meet consumer needs."} />
+
           </div>
           {/* <p className="mt-10 mb-4 md:text-5xl">
         
