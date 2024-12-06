d89d4fbb0dd2f4abbaa1ac088f2d475b3d0f2cfe
Zoya774 <158023579+Zoya774@users.noreply.github.com>
2024-12-06 11:53:53 +0530

    update categoriies

--- src/Components/PopularCategory.jsx
+++ src/Components/PopularCategory.jsx
@@ -106,20 +106,20 @@ import { Link } from "react-router-dom";
                 <div key={index} className="card-wrapper relative">
                   <div className="card relative text-white  flex flex-row-reverse items-start  h-[30vh] md:h-[70vh] w-[80vw] mx-auto rounded-lg shadow-lg overflow-hidden">
                     {/* Gradient Overlay */}
-                    <div className="absolute inset-0 bg-gradient-to-r from-dark via-dark to-transparent opacity-0 md:opacity-100 z-10"></div>
+                    <div className="absolute inset-0 bg-gradient-to-r from-dark via-dark to-transparent  md:opacity-100 z-10"></div>
 
                     {/* Image */}
                     <img
                       src={item.src}
                       alt={item.category}
-                      className="md:h-full md:w-[50vw] object-cover z-0"
+                      className="md:h-full md:w-[50vw] relative -right-32 object-cover z-0"
                     />
 
                     {/* Text Content */}
-                    <div className="p-4 relative z-20 py-40">
-                      <h1 className="text-5xl font-semibold head ">{item.text}</h1>
+                    <div className="p-4 absolute md:relative z-20 md:py-40">
+                      <h1 className="text-5xl font-semibold head  ">{item.text}</h1>
                       <p>{item.description}</p>
-                      <Link to={`/products/category/${item.category}`} > <button class="group relative  my-10 inline-flex h-20 w-20 items-center justify-center overflow-hidden rounded-full bg-neutral-950 font-medium text-neutral-200 transition-all duration-300 hover:w-60"><div class="inline-flex whitespace-nowrap opacity-0 transition-all duration-200 group-hover:-translate-x-3 group-hover:opacity-100">Know More</div><div class="absolute right-3.5"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-10 w-[60px]"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></div></button></Link>
+                      <Link to={`/products/category/${item.category}`} > <button class="group relative  my-10 inline-flex h-10 w-10 md:h-20 md:w-20 items-center justify-center overflow-hidden rounded-full bg-neutral-950 font-medium text-neutral-200 transition-all duration-300 hover:w-60"><div class="inline-flex whitespace-nowrap opacity-0 transition-all duration-200 group-hover:-translate-x-3 group-hover:opacity-100">Know More</div><div class="absolute right-3.5"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-10 w-[18px] md:w-[60px]"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></div></button></Link>
                     </div>
                     
                   </div>
