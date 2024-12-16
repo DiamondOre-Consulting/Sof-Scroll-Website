53d4d2194388a6c513b424b426b763c30514de11
Zoya774 <158023579+Zoya774@users.noreply.github.com>
2024-12-16 17:10:17 +0530

    added fly

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,7 +4,7 @@ import tisssuevideo from "../assets/tissuev.mp4";
 import AnimatedText from "./AnimatedText";
 import myimg from '../assets/2.png'
 import bg from '../assets/4.mp4'
-import fly from '../assets/fly.gif'
+import fly from '../assets/myfly.gif'
 
 const Hero = () => {
   return (
@@ -22,7 +22,7 @@ const Hero = () => {
       {/* Black Overlay */}
       {/* <div className="absolute bg-black inset-0 h-[100px] opacity-20"></div> */}
       <div className="absolute inset-0 bg-black opacity-10"></div>
-      {/* <img src={myimg} alt="" className="absolute inset-0 w-full" /> */}
+      <img src={fly} alt="" className="absolute w-96 inset-0  top-20" />
 
       <div className="container px-4 md:w-[60rem] flex justify-center flex-col items-center mx-auto font-bold text-white text-[2rem] -mt-40 head sm:text-[4rem]  md:text-[4rem] mf sm:px-2 ">
         <AnimatedText text2={"Luxury  You  Can  Trust"}  />
