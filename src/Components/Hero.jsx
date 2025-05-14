8e15409223116558b4ce2d973133570450d621be
DiamondOreDev <harsh.diamondore@gmail.com>
2025-05-14 17:12:16 +0530

    update hero page

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,45 +1,41 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import tisssuevideo from "../assets/tissuev.mp4";
-import AnimatedText from "./AnimatedText";
-import myimg from '../assets/2.png'
-import bg from '../assets/mainvideo.mp4'
-import fly from '../assets/myfly.gif'
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, EffectFade } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+import 'swiper/css/effect-fade';
 
 const Hero = () => {
-  return (
-    // <ShineBorder className={"w-full "}>
-    <>
-    <div className=" flex  items-center h-[100vh]  overflow-hidden bg-center bg-cover ">
-      <video
-         src={bg}
-        autoPlay
-        loop
-        muted
-        className="absolute inset-0 object-cover w-full h-full"
-      ></video>
+    const images = [
+        "https://res.cloudinary.com/disdsorfp/image/upload/v1747222429/WhatsApp_Image_2025-05-14_at_17.03.19_3afabf2f_eqqshw.jpg",
+        "https://res.cloudinary.com/disdsorfp/image/upload/v1747222429/WhatsApp_Image_2025-05-14_at_17.03.19_5cbefecc_ufrt82.jpg",
+        "https://res.cloudinary.com/disdsorfp/image/upload/v1747222429/WhatsApp_Image_2025-05-14_at_17.03.19_de868e19_qz7vw0.jpg",
+    ];
 
-      {/* Black Overlay */}
-      {/* <div className="absolute bg-black inset-0 h-[100px] opacity-20"></div> */}
-      <div className="absolute inset-0 bg-black opacity-10"></div>
-      {/* <img src={fly} alt="" className="absolute w-96 inset-0  top-20" /> */}
-
-      <div className="container px-4 md:w-[60rem] flex justify-center flex-col items-center mx-auto font-bold text-white text-[2rem] -mt-40 head sm:text-[4rem]  md:text-[4rem] mf sm:px-2 ">
-        <AnimatedText text2={"Luxury  You  Can  Trust"}  />
-        <AnimatedText text2={"Softness  You  Can  Feel"}  />
-
-
-
-      </div>
- 
-    </div>
-
-    {/* <img src="https://koshertissue.com/images/paper_borders-ai-min.png"   className=" relative  md:-mt-52   z-30 md:block hidden  -mt-44"  alt="" /> */}
-   
-
-    </>
-    // </ShineBorder>
-  );
-};
+  
+    return (
+        <div className='md:h-screen'>
+          <div className="md:absolute top-0 w-full mb-4">
+            <Swiper
+                modules={[Autoplay, Pagination, EffectFade]}
+                autoplay={{ delay: 3000, disableOnInteraction: false }}
+                pagination={{ clickable: true, type: 'bullets' }}
+                speed={2000}
+                loop={true}
+                effect="fade"
+                className="w-full h-full"
+            >
+                {images?.map((slide, ind) => (
+                    <SwiperSlide key={ind}>
+                        <img src={slide} className="w-full" alt={`Slide ${ind + 1}`} />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+        </div>
+    );
+}
 
 export default Hero;
